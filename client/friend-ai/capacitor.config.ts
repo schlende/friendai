33f89fb3f78af5ba5cc43718fdd2f48dcc919ca0
ForrestCogna <forrest@cogna.co>
2025-02-23 19:22:18 +0800

    remove friendai folder

--- client/friend-ai/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: "com.friendai.app",
-  appName: "FriendAI",
-  webDir: "dist",
-};
-
-export default config;
